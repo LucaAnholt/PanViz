1d07178c6c9479d9f9f5460fb03f3d88f89daade
Luca Anholt <la1317@ic.ac.uk>
2022-01-30 15:20:13 +0000

    adding decompose_IMON to namespace for export + improving function + adding better documentation + adding unit test

--- R/decompose_IMON.R
+++ R/decompose_IMON.R
@@ -1,17 +1,33 @@
-##algorithm to decompose non-connected IMONs
-#' IMON decomposer
+#' decompose_IMON
+#'
+#' @description This function returns a list of fully connected IMONs from a single parent unconnected IMON.
 #'
 #' @param G - igraph object containing non-fully connected IMON
 #'
-#' @return - list of igraph objects containing fully connected IMONs
+#' @return - list of igraph objects, where each index contains a fully connected IMON
+#' @export
+#' @examples \dontrun{
+#' G_list <- decompose_IMON(G)
+#' }
 decompose_IMON <- function(G){
+  ##check if user has provided IMON (igraph object)
+  if(missing(G)){
+    stop("Please provide an unnconnected graph (IMON)")
+  }
+  ##attempt decompose using igraph:
   d = igraph::decompose(G) #decompose graph
+  ##if the network is already fully connected, show error:
+  if(length(d) == 1){
+    stop("The IMON you provided is already fully connected! Cannot decompose!")
+  }
+  ##if the network is not fully connected, separate the networks into a list of fully connected networks that fit IMON definition:
   for(i in seq_along(d)){
     len = length(igraph::V(d[[i]])[grepl("SNP", igraph::V(d[[i]])$type)]$name) #remove any decomposed subgraph that doesn't contains snps
     if(len == 0){
       d[[i]] <- NA
     }
   }
+  ##remove any potential NA values:
   d = d[!is.na(d)]
   return(d)
 }
---
+++ tests/testthat/test_decompose_IMON.R
@@ -0,0 +1,12 @@
+library(PanViz)
+
+testthat::test_that("tests that decompose_IMON returns list object and throws correct errors on bad inputs", {
+  data("er_snp_vector")
+  G <- PanViz::get_IMON(snp_list = er_snp_vector)
+  G_list <- PanViz::decompose_IMON(G = G)
+  expect_equal(class(G_list), "list")
+  expect_equal(length(G_list), 9)
+  expect_equal(length(igraph::V(G_list[[1]])), 1750)
+  expect_error(PanViz::decompose_IMON(), "Please provide an unnconnected graph (IMON)", fixed = TRUE)
+  expect_error(PanViz::decompose_IMON(G_list[[1]]), "The IMON you provided is already fully connected! Cannot decompose!", fixed = TRUE)
+})
