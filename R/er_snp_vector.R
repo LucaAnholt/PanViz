327e92e3e33809c5dd65774eb4d141467fb577a3
Luca Anholt <la1317@ic.ac.uk>
2022-03-26 08:17:30 +0000

    adding line breaks to roxygen documentation for internal data

--- R/er_snp_vector.R
+++ R/er_snp_vector.R
@@ -1,6 +1,9 @@
-#' Summary-level GWAS data vector for estrogen-receptor positive breast cancer (EFO_1000649)
+#' Summary-level GWAS data vector for estrogen-receptor positive breast cancer
+#' (EFO_1000649)
 #'
-#' A dataset containing a vector of SNPs (summary-level GWAS data) associated with estrogen-receptor positive breast cancer (EFO_1000649), collated by the GWAS Catalog.
+#' A dataset containing a vector of SNPs (summary-level GWAS data) associated
+#' with estrogen-receptor positive breast cancer (EFO_1000649), collated by the
+#' GWAS Catalog.
 #'
 #' @docType data
 #' @keywords datasets
