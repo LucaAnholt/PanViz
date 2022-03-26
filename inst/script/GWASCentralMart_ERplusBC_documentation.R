64b14235ec2b9b25376d0f287fc97cf92552d4c7
Luca Anholt <la1317@ic.ac.uk>
2022-03-26 08:51:56 +0000

    adding documentation for inst/extdata

---
+++ inst/script/GWASCentralMart_ERplusBC_documentation.R
@@ -0,0 +1,23 @@
+#' Full summary-level GWAS data for estrogen-receptor positive breast cancer
+#' (meSH/HPO: D001943) downloaded from the GWAS Central on 8/2/2022
+#'
+#' A dataset containing a table of SNPs (summary-level GWAS data) associated
+#' with estrogen-receptor positive breast cancer (meSH/HPO: D001943), and various other
+#' study information, collated by the GWAS Central database.
+#'
+#' This data was sourced via the GWAS Central GWAS Mart API
+#' (https://mart.gwascentral.org/), where the following filters and attributes
+#' were applied:
+#'
+#' - Under "Filter", a filter for meSH/HPO Phenotype Identifier D001943 was applied,
+#' corresponding to estrogen postive breast cancer.
+#' - Under "Attributes", "Study Name" and "Annotation Name" was selected in STUDY
+#' INFORMATION and HGNC Gene Symbol selected under ASSOCIATION RESULTS
+#'
+#'
+#' @docType data
+#' @keywords external dataset
+#' @references https://mart.gwascentral.org/
+#' @usage PanViz::GWAS_data_reader(file = system.file("extdata", "GWASCentralMart_ERplusBC.tsv", package="PanViz"), snp_col = "Source Marker Accession", study_col = "Study Name", trait_col = "Annotation Name")
+#' @format A tab separated file with 112 rows and 38 columns
+"GWASCentralMart_ERplusBC.tsv"
---
+++ inst/script/gwas-association-downloaded_2021-09-13-EFO_1000649_documentation.R
@@ -0,0 +1,13 @@
+#' Full summary-level GWAS data for estrogen-receptor positive breast cancer
+#' (EFO_1000649) downloaded from the GWAS Catalog on 13/9/2021
+#'
+#' A dataset containing a table of SNPs (summary-level GWAS data) associated
+#' with estrogen-receptor positive breast cancer (EFO_1000649), and various other
+#' study information, collated by the GWAS Catalog.
+#'
+#' @docType data
+#' @keywords external dataset
+#' @references https://www.ebi.ac.uk/gwas/efotraits/EFO_1000649
+#' @usage PanViz::GWAS_data_reader(file = system.file("extdata", "gwas-association-downloaded_2021-09-13-EFO_1000649.tsv", package="PanViz"), snp_col = "SNPS", study_col = "STUDY", trait_col = "DISEASE/TRAIT")
+#' @format A tab separated file with 112 rows and 38 columns
+"gwas-association-downloaded_2021-09-13-EFO_1000649.tsv"
