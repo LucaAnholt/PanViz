b791e3a1d2fbb7058fe5f9aad49383d11d38d15a
Luca Anholt <la1317@ic.ac.uk>
2022-04-02 16:11:32 +0100

    fixing append errors

--- R/Get_Kegg_Data.R
+++ R/Get_Kegg_Data.R
@@ -9,7 +9,7 @@
 #' @importFrom usethis use_data
 #' @export
 #' @example
-#' get_KEGG_data(CPU = 2, sleep = 5)
+#' get_Kegg_data(CPU = 2, sleep = 5)
 get_Kegg_data <- function(CPU = 1, sleep = 5){
   if(CPU > 2){
     stop("Can only use a maximum of 2 workers")
@@ -17,10 +17,10 @@ get_Kegg_data <- function(CPU = 1, sleep = 5){
   ##Querying KEGG for metabolite, reaction and enzyme data:
   data <- retry(Reactions_Get_All(CPU = CPU, sleep = sleep), maxErrors = 3, sleep = sleep)
   adjl_R_E <- append(data[[1]], adjl_R_E)
-  adjl_RP_C <- append(data[[2]], adjl_R_E)
+  adjl_RP_C <- append(data[[2]], adjl_RP_C)
   adjl_RP_R <- append(data[[3]], adjl_RP_R)
   ##Querying compound names as hash:
   compound_names_hash <- get_compound_hashmap()
   ##save to internal sys data:
-  usethis::use_data(adjl_G_E, adjl_R_E, adjl_RP_C, adjl_RP_R, Gene_Locations, compound_names_hash, internal = TRUE, overwrite = TRUE)
+  usethis::use_data(adjl_R_E, adjl_RP_C, adjl_RP_R, compound_names_hash, internal = TRUE, overwrite = TRUE)
 }
