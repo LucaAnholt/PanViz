20e867333b400d1ed23ebacee0ae4c99785e06be
Luca Anholt <la1317@ic.ac.uk>
2022-04-02 15:40:44 +0100

    add function example

--- R/Get_Kegg_Data.R
+++ R/Get_Kegg_Data.R
@@ -8,6 +8,8 @@
 #' @return Rds files containing adjacency lists for all KEGG network data
 #' @importFrom usethis use_data
 #' @export
+#' @example
+#' get_KEGG_data(CPU = 2, sleep = 5)
 get_Kegg_data <- function(CPU = 1, sleep = 5){
   if(CPU > 2){
     stop("Can only use a maximum of 2 workers")
